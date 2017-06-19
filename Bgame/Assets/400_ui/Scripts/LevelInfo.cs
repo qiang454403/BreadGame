19db3ca3c8ef3fc799c67b4c14b016d539d2af97
gouzhiqiang <zhiqiang.gou@ceeyoo.net>
2017-06-19 18:27:01 +0800

    关卡信息

---
+++ Bgame/Assets/400_ui/Scripts/LevelInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelInfo
+{
+	public int LevelID;
+	public bool IsLock;
+	public int Level_Score;
+	public float Level_All_Time;
+	public float Customer_Interval;
+	
+}
